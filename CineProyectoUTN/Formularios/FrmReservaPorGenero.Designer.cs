7a525adb1c1e6323e4cfb4e28aab37c0e0a44a01
Gabriel Ortiz <gortiz96sanchez@gmail.com>
2022-10-27 13:26:31 -0300

    FrmReservaxGenero

--- CineProyectoUTN/Formularios/FrmReservaPorGenero.Designer.cs
+++ CineProyectoUTN/Formularios/FrmReservaPorGenero.Designer.cs
@@ -29,17 +29,12 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fecha_reserva = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.pelicula = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cboGenero = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -51,12 +46,6 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.cliente,
-            this.edad,
-            this.fecha_reserva,
-            this.pelicula,
-            this.genero});
             this.dataGridView1.Location = new System.Drawing.Point(12, 60);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
@@ -64,36 +53,6 @@
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(503, 419);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // cliente
-            // 
-            this.cliente.HeaderText = "Cliente";
-            this.cliente.Name = "cliente";
-            this.cliente.ReadOnly = true;
-            // 
-            // edad
-            // 
-            this.edad.HeaderText = "Edad";
-            this.edad.Name = "edad";
-            this.edad.ReadOnly = true;
-            // 
-            // fecha_reserva
-            // 
-            this.fecha_reserva.HeaderText = "Fecha Reserva";
-            this.fecha_reserva.Name = "fecha_reserva";
-            this.fecha_reserva.ReadOnly = true;
-            // 
-            // pelicula
-            // 
-            this.pelicula.HeaderText = "Pelicula";
-            this.pelicula.Name = "pelicula";
-            this.pelicula.ReadOnly = true;
-            // 
-            // genero
-            // 
-            this.genero.HeaderText = "Genero";
-            this.genero.Name = "genero";
-            this.genero.ReadOnly = true;
             // 
             // label1
             // 
@@ -142,13 +101,13 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Genero pelicula:";
             // 
-            // comboBox1
+            // cboGenero
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(563, 179);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 23);
-            this.comboBox1.TabIndex = 6;
+            this.cboGenero.FormattingEnabled = true;
+            this.cboGenero.Location = new System.Drawing.Point(563, 179);
+            this.cboGenero.Name = "cboGenero";
+            this.cboGenero.Size = new System.Drawing.Size(173, 23);
+            this.cboGenero.TabIndex = 6;
             // 
             // button1
             // 
@@ -159,6 +118,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -169,6 +129,7 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FrmReservaPorGenero
             // 
@@ -177,7 +138,7 @@
             this.ClientSize = new System.Drawing.Size(800, 491);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cboGenero);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label3);
@@ -186,6 +147,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "FrmReservaPorGenero";
             this.Text = "FrmReservaPorGenero";
+            this.Load += new System.EventHandler(this.FrmReservaPorGenero_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
@@ -196,17 +158,12 @@
         #endregion
 
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn cliente;
-        private DataGridViewTextBoxColumn edad;
-        private DataGridViewTextBoxColumn fecha_reserva;
-        private DataGridViewTextBoxColumn pelicula;
-        private DataGridViewTextBoxColumn genero;
         private Label label1;
         private Label label2;
         private Label label3;
         private NumericUpDown numericUpDown1;
         private Label label4;
-        private ComboBox comboBox1;
+        private ComboBox cboGenero;
         private Button button1;
         private Button button2;
     }
--- CineProyectoUTN/Formularios/FrmReservaPorGenero.cs
+++ CineProyectoUTN/Formularios/FrmReservaPorGenero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CineProyectoUTN.Datos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,47 @@ namespace CineProyectoUTN.Formularios
 {
     public partial class FrmReservaPorGenero : Form
     {
+        List<Parametro> lParametro = new List<Parametro>();
+        Helper helper = new Helper();
+        Parametro pEdad;
+        Parametro pGenero;
         public FrmReservaPorGenero()
         {
             InitializeComponent();
+        }
+
+        private void FrmReservaPorGenero_Load(object sender, EventArgs e)
+        {
+            cargarCbo(cboGenero, "SELECT * FROM Generos_peliculas", "nombre_genero", "id_genero_pelicula");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CargarConsulta();
+            button1.Enabled = false;
+        }
+
+        private void CargarConsulta()
+        {
+            string sp = "sp_reservas_terror";
+            pGenero = new Parametro("@Genero", cboGenero.Text);
+            pEdad = new Parametro("@edad", numericUpDown1.Value);
+            lParametro.Add(pGenero);
+            lParametro.Add(pEdad);
+
+            DataTable table = helper.ConsultaSQLSP(sp, lParametro);
+            dataGridView1.DataSource = table;
+        }
+        private void cargarCbo(ComboBox cbo, string select, string display, string value)
+        {
+            cbo.DataSource = helper.consultaSql(select);
+            cbo.DisplayMember = display;
+            cbo.ValueMember = value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dispose();
         }
     }
 }
